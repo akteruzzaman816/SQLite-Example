c88408a0865c65ec78d5a44553c9fb00d25be37c
AkterUzzaman <akteruzzaman816@gmail.com>
2020-03-10 17:03:13 +0600

    SQLite Example

---
+++ app/src/main/java/com/bdtask/sqliteexample/Constants.java
@@ -0,0 +1,33 @@
+package com.bdtask.sqliteexample;
+
+import android.database.sqlite.SQLiteDatabase;
+
+public class Constants {
+
+
+    public static final String DATABASE_NAME      = "note.db";
+    public static final int DATABASE_VERSION      = 2;
+    public static final String TABLE_NAME         = "notes";
+    public static final String COLUMN_ID          = "id";
+    public static final String COLUMN_TITLE       = "title";
+    public static final String COLUMN_DESCRIPTION = "description";
+    public static final String COLUMN_DATE        = "date";
+
+
+
+
+
+    public static String CREATE_TABLE_QUERY ="CREATE TABLE "+TABLE_NAME+"("
+            +COLUMN_ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "
+            +COLUMN_TITLE+" TEXT, "
+            +COLUMN_DATE+" TEXT, "
+            +COLUMN_DESCRIPTION+ " TEXT"
+            +")";
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/bdtask/sqliteexample/MainActivity.java
@@ -0,0 +1,143 @@
+package com.bdtask.sqliteexample;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FloatingActionButton addButton;
+    DatabaseHelper databaseHelper;
+    CustomAdapter adapter;
+    List<Note> dataList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView           = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        addButton              = findViewById(R.id.addButton);
+        databaseHelper         = new DatabaseHelper(MainActivity.this);
+
+
+        loadData();
+
+
+
+
+
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                customDialog();
+
+            }
+        });
+
+
+    }
+
+    private void loadData() {
+
+        dataList  = new ArrayList<>();
+        dataList = databaseHelper.getAllNotes();
+
+        if (dataList.size() > 0){
+            adapter = new CustomAdapter(MainActivity.this,dataList);
+            recyclerView.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
+        }else {
+            Toast.makeText(this, "No data found", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
+    }
+
+
+    private void customDialog() {
+
+        AlertDialog.Builder builder  = new AlertDialog.Builder(MainActivity.this);
+        View view = getLayoutInflater().inflate(R.layout.custom_dialog,null);
+        builder.setView(view);
+
+
+        final AlertDialog alertDialog = builder.create();
+
+        final EditText title       = view.findViewById(R.id.title);
+        final EditText description = view.findViewById(R.id.description);
+
+        Button saveButton    = view.findViewById(R.id.saveButton);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String titleValue       = title.getText().toString();
+                String descriptionValue = description.getText().toString();
+
+                if (titleValue.isEmpty()){
+                    title.setError("Enter Title");
+                    return;
+                }else if (descriptionValue.isEmpty()){
+                    description.setError("Enter description");
+                    return;
+                }
+
+                Calendar calendar = Calendar.getInstance();
+                SimpleDateFormat format = new SimpleDateFormat("MM-dd");
+                String date = format.format(calendar.getTimeInMillis());
+
+
+
+                long status = databaseHelper.insertData(new Note(titleValue,descriptionValue,date));
+                if (status != -1){
+                    alertDialog.dismiss();
+                    loadData();
+                    Toast.makeText(MainActivity.this, "Successfully Inserted", Toast.LENGTH_SHORT).show();
+                }else {
+                    alertDialog.dismiss();
+                    Toast.makeText(MainActivity.this, "Failed to Insert", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+
+
+            }
+        });
+
+
+
+
+
+
+        alertDialog.show();
+
+
+
+
+
+    }
+}
